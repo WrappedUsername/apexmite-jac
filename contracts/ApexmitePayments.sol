dfa314c93541638a0283266e32ce343196be8ea9
WrappeUsername <wrappedusername@gmail.com>
2023-05-18 18:01:52 -0600

    Configuring files - Keefe

--- contracts/ApexmitePayments.sol
+++ contracts/ApexmitePayments.sol
@@ -4,16 +4,20 @@ pragma solidity 0.8.9;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ApexmitePayments is Ownable {
-    uint256 public price;
+    uint256 public price = 0.0025 ether;
 
+    event PaymentReceived(address indexed payer, uint256 amount);
     event Withdrawal(uint amount, uint when);
 
-    constructor(uint256 _price) {
-        price = _price;
-    }
+    constructor() {}
 
     function paySubscription() public payable {
         require(msg.value == price, "Incorrect payment amount");
+        emit PaymentReceived(msg.sender, msg.value);
+    }
+
+    function updateSubscriptionPrice(uint256 newSubscriptionPrice) public onlyOwner {
+        price = newSubscriptionPrice;
     }
 
     function withdraw() public onlyOwner {
